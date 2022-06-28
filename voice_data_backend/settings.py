6db0af2b7b185c021711d610ff0ce58e9c171cfd
fazledyn <1705066@ugrad.cse.buet.ac.bd>
2022-06-28 18:31:59 +0600

    Added trusted origins

--- voice_data_backend/settings.py
+++ voice_data_backend/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.web.app', 'https://*.buetcsefest2022.com', 'https://*.herokuapp.com']
 
 # Application definition
 
